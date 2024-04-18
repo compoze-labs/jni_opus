e79ef57264e8ffb5e037736d8b6b2c6f761fc3d2
Jon Christensen <jon.christensen@compozelabs.com>
2024-04-18 08:18:04 -0500

    add compoze.sh
    adds script to copy .a and .dylib to /usr/local/include on Mac (and verify that the dir exists)

---
+++ compoze.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+libs_dir="./.libs"
+include_dir="/usr/local/include/"
+
+function verify_include_dir() {
+    if [ ! -d "$include_dir" ]; then
+      mkdir -p "$include_dir"
+    fi
+}
+
+function copy_lib(){
+      cp "$libs_dir/$1" "$include_dir"
+}
+
+verify_include_dir
+copy_lib "libopus.a"
+copy_lib "libopus.0.dylib"
+
