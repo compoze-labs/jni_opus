7b6e431f23374fed67a46e063ced142a16773b26
Jon Christensen <jon.christensen@compozelabs.com>
2024-04-24 06:55:52 -0500

    split + encode/decode

--- include/opus_jni.h
+++ include/opus_jni.h
@@ -1,38 +1,38 @@
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 //define something for Windows (32-bit and 64-bit, this part is common)
-   #ifdef _WIN64
-        #include "windows/jawt_md.h"
-        #include "windows/jni.h"
-        #include "windows/jni_md.h"
+#ifdef _WIN64
+#include "windows/jawt_md.h"
+#include "windows/jni.h"
+#include "windows/jni_md.h"
       //define something for Windows (64-bit only)
-   #else
+#else
       //define something for Windows (32-bit only)
-   #endif
+#endif
 #elif __APPLE__
 
-    #include "macos/jawt_md.h"
-    #include "macos/jni.h"
-    #include "macos/jni_md.h"
+#include "macos/jawt_md.h"
+#include "macos/jni.h"
+#include "macos/jni_md.h"
 
 #include <TargetConditionals.h>
-    #if TARGET_IPHONE_SIMULATOR
-         // iOS, tvOS, or watchOS Simulator
-    #elif TARGET_OS_MACCATALYST
-         // Mac's Catalyst (ports iOS API into Mac, like UIKit).
-    #elif TARGET_OS_IPHONE
-        // iOS, tvOS, or watchOS device
-    #elif TARGET_OS_MAC
-        // Other kinds of Apple platforms
-    #else
-    #   error "Unknown Apple platform"
-    #endif
+#if TARGET_IPHONE_SIMULATOR
+// iOS, tvOS, or watchOS Simulator
+#elif TARGET_OS_MACCATALYST
+// Mac's Catalyst (ports iOS API into Mac, like UIKit).
+#elif TARGET_OS_IPHONE
+// iOS, tvOS, or watchOS device
+#elif TARGET_OS_MAC
+// Other kinds of Apple platforms
+#else
+#   error "Unknown Apple platform"
+#endif
 #elif __ANDROID__
 // Below __linux__ check should be enough to handle Android,
     // but something may be unique to Android.
 #elif __linux__
 // linux
-#elif __unix__ // all unices not caught above
+#elif __unix__
 // Unix
 #elif defined(_POSIX_VERSION)
 // POSIX
@@ -42,9 +42,5 @@
 
 #ifndef JNI_OPUS_OPUS_JNI_H
 #define JNI_OPUS_OPUS_JNI_H
-
-//int get100(void);
-//int get1000(void);
-JNIEXPORT int JNICALL Java_HelloJNI_sayHello(JNIEnv *, jobject);
 
 #endif //JNI_OPUS_OPUS_JNI_H
--- src/opus_jni.c
+++ src/opus_jni.c
@@ -1,4 +1,3 @@
-//#include <jni.h> // can I have opus.h handle this 1x...?
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,10 +8,69 @@ static const char *JNIT_CLASS = "interop/PrintLine";
 
 char logMsg[255];
 OpusDecoder *dec;
+OpusEncoder *enc;
+
 
 opus_int32 SAMPLING_RATE;
 int CHANNELS;
 int FRAME_SIZE;
+
+int APPLICATION_TYPE = OPUS_APPLICATION_VOIP;
+const int MAX_PAYLOAD_BYTES = 1500;
+
+
+static jboolean opus_init_encoder (JNIEnv *env, jobject obj, jint samplingRate, jint numberOfChannels, jint frameSize)
+{
+    FRAME_SIZE = frameSize;
+    SAMPLING_RATE = samplingRate;
+    CHANNELS = numberOfChannels;
+
+    int error;
+    int size;
+
+    size = opus_encoder_get_size(1);
+    enc = malloc(size);
+    error = opus_encoder_init(enc, SAMPLING_RATE, CHANNELS, APPLICATION_TYPE);
+
+    sprintf(logMsg, "Initialized Encoder with ErrorCode: %d", error);
+
+    return error;
+}
+
+static jint opus_encode_bytes (JNIEnv *env, jobject obj, jshortArray in, jbyteArray out)
+{
+    jint inputArraySize = (*env)->GetArrayLength(env, in);
+    jint outputArraySize = (*env)->GetArrayLength(env, out);
+
+    jshort* audioSignal = (*env)->GetShortArrayElements(env, in, 0);
+
+    unsigned char *data = (unsigned char*)calloc(MAX_PAYLOAD_BYTES,sizeof(unsigned char));
+    int dataArraySize = opus_encode(enc, audioSignal, FRAME_SIZE, data, MAX_PAYLOAD_BYTES);
+
+    if (dataArraySize >=0)
+    {
+        if (dataArraySize <= outputArraySize)
+        {
+            (*env)->SetByteArrayRegion(env,out,0,dataArraySize,data);
+        }
+        else
+        {
+            sprintf(logMsg, "Output array of size: %d to small for storing encoded data.", outputArraySize);
+            return -1;
+        }
+    }
+
+    (*env)->ReleaseShortArrayElements(env,in,audioSignal,JNI_ABORT);
+
+    return dataArraySize;
+}
+
+static jboolean opus_release_encoder (JNIEnv *env, jobject obj)
+{
+    free(enc);
+
+    return 1;
+}
 
 static jboolean opus_init_decoder (JNIEnv *env, jobject obj, jint samplingRate, jint numberOfChannels, jint frameSize){
     FRAME_SIZE = frameSize;
@@ -62,6 +120,9 @@ static jboolean opus_release_decoder (JNIEnv *env, jobject obj){
 
 
 static JNINativeMethod funcs[] = {
+        { "opus_init_decoder", "(III)Z", (void *)&opus_init_encoder },
+        { "opus_decode_bytes", "([B[S)I", (void *)&opus_encode_bytes },
+        { "opus_release_decoder", "()Z", (void *)&opus_release_encoder },
         { "opus_init_decoder", "(III)Z", (void *)&opus_init_decoder },
         { "opus_decode_bytes", "([B[S)I", (void *)&opus_decode_bytes },
         { "opus_release_decoder", "()Z", (void *)&opus_release_decoder }
